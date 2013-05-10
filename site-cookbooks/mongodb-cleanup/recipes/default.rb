e4bf63e1f0ccec3135958bda67dbb5c4da4f9136
eba <h-ebata@xseed.co.jp>
2013-05-10 18:08:51 +0900

    mongodb-cleanup recipes

--- site-cookbooks/mongodb-cleanup/recipes/default.rb
+++ site-cookbooks/mongodb-cleanup/recipes/default.rb
@@ -6,39 +6,25 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-bash "setup-mongo" do
+bash "mongodb-cleanup" do
         user "root"
 	flags "-e"
         code <<-EOH
-        #MongoDB 停止
         mongo admin --eval "db.shutdownServer()"
-
-        #mongod.conf 修正
         sed -i "s/^replSet/#replSet/g" /etc/mongod.conf
-
-        #lockファイルの削除
         if [ -e /var/lib/mongo/mongod.lock ]; then
                 rm -r /var/lib/mongo/mongod.lock
         fi
 
-        #MongoDB 起動
         /etc/init.d/mongod start
-
-        #Database 削除
         mongo adfurikun-prd --eval "db.dropDatabase()"
-
-        #MongoDB 停止
         mongo admin --eval "db.shutdownServer()"
-
-        #mongod.conf 修正
         sed -i "s/^#replSet/replSet/g" /etc/mongod.conf
 
-        #lockファイル削除
         if [ -e /var/lib/mongo/mongod.lock ]; then
                 rm -r /var/lib/mongo/mongod.lock
         fi
 
-        #MongoDB 起動
         /etc/init.d/mongod start
         EOH
 end
